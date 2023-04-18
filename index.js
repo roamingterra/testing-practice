316bae27caf4f6c2a924d02672c2a83181224098
roamingterra <danielballerini1@gmail.com>
2023-04-17 21:13:27 -0400

    Write caesarCipher function
    I need to also add that there were typos with the 4th test. The expected
    results for the last three out of four sub tests were meant to be a
    (whether capitalized or not) instead of z. I am still getting used to
    TDD and realize that tests should not have typos while attempting to
    write the function to test.

--- index.js
+++ index.js
@@ -23,4 +23,42 @@ const calculator = {
   },
 };
 
-export { capitalizeFirstLetter, reverseString, calculator };
+function caesarCipher(input, shiftFactor) {
+  const inputTest = input.split("");
+  const punctuationRegex = /[!"#$%&'()*+,\-./:;<=>?@[\\\]^_`{|}~]/g;
+  let result = [];
+
+  for (let i = 0; i < inputTest.length; i++) {
+    if (inputTest[i] === " ") {
+      result.push(inputTest[i]);
+    } else if (inputTest[i].match(punctuationRegex)) {
+      result.push(inputTest[i]);
+    } else if (
+      inputTest[i].charCodeAt(0) >= 97 &&
+      inputTest[i].charCodeAt(0) <= 122
+    ) {
+      let shiftedNum;
+      shiftedNum = inputTest[i].charCodeAt(0) - 97; // Convert to 0-based index
+      shiftedNum = (shiftedNum + shiftFactor) % 26; // Apply the shift with modulo operation
+      shiftedNum = (shiftedNum + 26) % 26; // Handle negative values after modulo
+      shiftedNum = shiftedNum + 97; // Convert back to ASCII code
+
+      result.push(String.fromCharCode(shiftedNum));
+    } else if (
+      inputTest[i].charCodeAt(0) >= 65 &&
+      inputTest[i].charCodeAt(0) <= 90
+    ) {
+      let shiftedNum;
+      shiftedNum = inputTest[i].charCodeAt(0) - 65; // Convert to 0-based index
+      shiftedNum = (shiftedNum + shiftFactor) % 26; // Apply the shift with modulo operation
+      shiftedNum = (shiftedNum + 26) % 26; // Handle negative values after modulo
+      shiftedNum = shiftedNum + 65; // Convert back to ASCII code
+
+      result.push(String.fromCharCode(shiftedNum));
+    }
+  }
+  result = result.join("");
+  return result;
+}
+
+export { capitalizeFirstLetter, reverseString, calculator, caesarCipher };
--- index.test.js
+++ index.test.js
@@ -41,7 +41,6 @@ test.skip("calculator object to perform all basic math operations", () => {
 
 // TEST #4
 test("shift all letters of a string by one letter", () => {
-  // Define test parameters
   const input1 = "Defend z east wall of z castle!";
   const shift1 = 1;
   const cipherText1 = caesarCipher(input1, shift1);
@@ -50,58 +49,15 @@ test("shift all letters of a string by one letter", () => {
   const input2 = "a";
   const shift2 = 26;
   const cipherText2 = caesarCipher(input2, shift2);
-  expect(cipherText2).toBe("z");
+  expect(cipherText2).toBe("a");
 
   const input3 = "A";
   const shift3 = 26;
   const cipherText3 = caesarCipher(input3, shift3);
-  expect(cipherText3).toBe("Z");
+  expect(cipherText3).toBe("A");
 
   const input4 = "a";
   const shift4 = 52;
   const cipherText4 = caesarCipher(input4, shift4);
-  expect(cipherText4).toBe("z");
-
-  /*
-  // Perform test
-  const inputTest = input.split("");
-  const cipherTextTest = cipherText.split("");
-
-  const punctuationRegex = /[!"#$%&'()*+,\-./:;<=>?@[\\\]^_`{|}~]/g;
-
-  expect(inputTest.length).toEqual(cipherTextTest.length);
-  for (let i = 0; i < cipherTextTest.length; i++) {
-    if (inputTest[i] === " ") {
-      expect(inputTest[i] === cipherTextTest[i]);
-      continue;
-    }
-
-    if (inputTest[i].match(punctuationRegex)) {
-      expect(inputTest[i] === cipherTextTest[i]);
-      continue;
-    }
-
-    if (inputTest[i].charCodeAt(0) >= 97 && inputTest[i].charCodeAt(0) <= 122) {
-      let shiftedNum;
-      shiftedNum = inputTest[i].charCodeAt(0) - 97; // Convert to 0-based index
-      shiftedNum = (shiftedNum + shift) % 26; // Apply the shift with modulo operation
-      shiftedNum = (shiftedNum + 26) % 26; // Handle negative values after modulo
-      shiftedNum = shiftedNum + 97; // Convert back to ASCII code
-
-      expect(String.fromCharCode(shiftedNum)).toEqual(cipherTextTest[i]);
-      continue;
-    }
-
-    if (inputTest[i].charCodeAt(0) >= 65 && inputTest[i].charCodeAt(0) <= 90) {
-      let shiftedNum;
-      shiftedNum = inputTest[i].charCodeAt(0) - 65; // Convert to 0-based index
-      shiftedNum = (shiftedNum + shift) % 26; // Apply the shift with modulo operation
-      shiftedNum = (shiftedNum + 26) % 26; // Handle negative values after modulo
-      shiftedNum = shiftedNum + 65; // Convert back to ASCII code
-
-      expect(String.fromCharCode(shiftedNum)).toEqual(cipherTextTest[i]);
-      continue;
-    }
-  }
-  */
+  expect(cipherText4).toBe("a");
 });
